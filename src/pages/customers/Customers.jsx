81ef2eb8fa4f72a18bf6dbbcfa37beed196209a1
franklin-raph <igboekwulusifranklin@gmail.com>
2024-05-15 02:53:58 +0100

    implemented react pagination

--- src/pages/customers/Customers.jsx
+++ src/pages/customers/Customers.jsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from 'react'
 import CustomerDropDown from '../../components/customer-drop-down/CustomerDropDown'
 import { useNavigate } from 'react-router-dom'
+import ReactPaginate from 'react-paginate'
 
 const Customers = ({baseUrl}) => {
 
@@ -34,6 +35,57 @@ const Customers = ({baseUrl}) => {
     getAllCustomers()
   },[])
 
+  const [pageNumber, setPageNumber] = useState(0)
+
+  const usersPerPage = 5
+  const visitedPages = pageNumber * usersPerPage
+
+  const displayUsers = allCustomers
+        .slice(visitedPages, visitedPages + usersPerPage)
+        .filter((customer) => {
+        if (searchString === "") return customer
+        else if (customer.first_name.toLowerCase().includes(searchString.toLowerCase()) 
+          || customer.last_name.toLowerCase().includes(searchString.toLowerCase()) 
+          || customer.last_name.toLowerCase().includes(searchString.toLowerCase())) return customer
+        })
+        .map((customer, index) => {
+            return(
+              <tr class="bg-white border-b cursor-pointer" onClick={e => navigate(`/customer/${customer.id}`)}>
+                <td class="pl-3 pr-6 py-4">
+                  {index + 1 }
+                </td>
+                <td class="pl-3 pr-6 py-4">
+                  {customer.first_name} {customer.last_name}
+                </td>
+                <td class="px-6 py-4">
+                  {new Date(customer.date_joined).toDateString()}
+                </td>
+                <td class="px-6 py-4">
+                  {new Date(customer.last_seen).toDateString()}
+                </td>
+                <td class={`px-6 py-4 capitalize ${
+                  customer.kyc_status === 'not_set' ? 'text-gray-500' :
+                  customer.kyc_status === 'pending' ? 'text-yellow-500' :
+                  customer.kyc_status === 'rejected' ? 'text-red-500' : 'text-green-500'
+                }`}>
+                  {customer.kyc_status}
+                </td>
+                <td class="px-6 py-4">
+                  {customer.email}
+                </td>
+                <td class="px-6 py-4">
+                  {customer.phone}
+                </td>
+            </tr>
+            )
+        })
+
+    const pageCount = Math.ceil(allCustomers.length / usersPerPage)
+
+    const changePage  = ({selected}) => {
+        setPageNumber(selected)
+    }
+
   return (
     <div className='shadow bg-white rounded-[20px] p-[30px]'>
       <p className='text-[#333333] text-[20px] font-[700]'>Customers <span className='text-[#A1A1A1] font-[400]'>(245)</span> </p>
@@ -56,6 +108,9 @@ const Customers = ({baseUrl}) => {
               <thead class="text-[14px] text-[#5C5C5C] capitalize border-b">
                   <tr>
                       <th scope="col" class="pl-3 pr-6 py-3">
+                          S/N
+                      </th>
+                      <th scope="col" class="pl-3 pr-6 py-3">
                           Customers
                       </th>
                       <th scope="col" class="px-6 py-3">
@@ -77,49 +132,50 @@ const Customers = ({baseUrl}) => {
               </thead>
               <tbody>
                 {
-                  allCustomers && allCustomers.filter((customer) => {
-                    if (searchString === "") return customer
-                    else if (customer.first_name.toLowerCase().includes(searchString.toLowerCase()) 
-                              || customer.last_name.toLowerCase().includes(searchString.toLowerCase()) 
-                              || customer.last_name.toLowerCase().includes(searchString.toLowerCase())) return customer
-                }).map(customer => (
-                    <tr class="bg-white border-b cursor-pointer" onClick={e => navigate(`/customer/${customer.id}`)}>
-                        <td class="pl-3 pr-6 py-4">
-                          {customer.first_name} {customer.last_name}
-                        </td>
-                        <td class="px-6 py-4">
-                          {new Date(customer.date_joined).toDateString()}
-                        </td>
-                        <td class="px-6 py-4">
-                          {new Date(customer.last_seen).toDateString()}
-                        </td>
-                        <td class={`px-6 py-4 capitalize ${
-                          customer.kyc_status === 'not_set' ? 'text-gray-500' :
-                          customer.kyc_status === 'pending' ? 'text-yellow-500' :
-                          customer.kyc_status === 'rejected' ? 'text-red-500' : 'text-green-500'
-                        }`}>
-                          {customer.kyc_status}
-                        </td>
-                        <td class="px-6 py-4">
-                          {customer.email}
-                        </td>
-                        <td class="px-6 py-4">
-                          {customer.phone}
-                        </td>
-                        {/* <td class="px-1 py-4 cursor-pointer" onClick={() => setShowCustomerDropDown(!showCustomerDropDown)}>
-                          :
-                        </td>
-                        {
-                          showCustomerDropDown &&
-                          <div className='absolute top-[50px] right-[15px]'>
-                            <CustomerDropDown />
-                          </div>
-                        } */}
-                    </tr>
-                  ))
+                  displayUsers
+                //   allCustomers && allCustomers.filter((customer) => {
+                //     if (searchString === "") return customer
+                //     else if (customer.first_name.toLowerCase().includes(searchString.toLowerCase()) 
+                //               || customer.last_name.toLowerCase().includes(searchString.toLowerCase()) 
+                //               || customer.last_name.toLowerCase().includes(searchString.toLowerCase())) return customer
+                // }).map(customer => (
+                //     // <tr class="bg-white border-b cursor-pointer" onClick={e => navigate(`/customer/${customer.id}`)}>
+                //     //     <td class="pl-3 pr-6 py-4">
+                //     //       {customer.first_name} {customer.last_name}
+                //     //     </td>
+                //     //     <td class="px-6 py-4">
+                //     //       {new Date(customer.date_joined).toDateString()}
+                //     //     </td>
+                //     //     <td class="px-6 py-4">
+                //     //       {new Date(customer.last_seen).toDateString()}
+                //     //     </td>
+                //     //     <td class={`px-6 py-4 capitalize ${
+                //     //       customer.kyc_status === 'not_set' ? 'text-gray-500' :
+                //     //       customer.kyc_status === 'pending' ? 'text-yellow-500' :
+                //     //       customer.kyc_status === 'rejected' ? 'text-red-500' : 'text-green-500'
+                //     //     }`}>
+                //     //       {customer.kyc_status}
+                //     //     </td>
+                //     //     <td class="px-6 py-4">
+                //     //       {customer.email}
+                //     //     </td>
+                //     //     <td class="px-6 py-4">
+                //     //       {customer.phone}
+                //     //     </td>
+                //     // </tr>
+                //   ))
                 }
               </tbody>
           </table>
+          <ReactPaginate
+              previousLabel={'Prev'}
+              nextLabel = {'Next'}
+              pageCount={pageCount}
+              onPageChange={changePage}
+              containerClassName='flex items-center gap-9 mt-5 justify-end pr-[30px] paginationBtns'
+              activeClassName='bg-secondary-color text-white'
+              disabledClassName='bg-gray-500 cursor-not-allowed'
+          />
       </div>
 
     </div>
