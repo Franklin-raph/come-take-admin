921fba3aebb0366445f2e1471bac89becf6788f2
franklin-raph <igboekwulusifranklin@gmail.com>
2025-02-17 09:37:24 +0100

    update

--- src/pages/transactions/Transactions.jsx
+++ src/pages/transactions/Transactions.jsx
@@ -2,11 +2,12 @@ import React, { useEffect, useState } from 'react'
 
 const Transactions = () => {
 
-  const transactionsArray = ['All', 'Confirmed', 'Pending', 'Failed']
+  // const transactionsArray = ['All', 'Confirmed', 'Pending', 'Failed']
 
   const admin = JSON.parse(localStorage.getItem('admin'))
   const [allTransactions, setAllTransactions] = useState([])
   const [loading, setLoading] = useState(false)
+  const [searchString, setSearchString] = useState('');
 
   async function getAllTransactions(){
     const res = await fetch(`https://cometakebe.onrender.com/administrator/dashboard/transactions`,{
@@ -27,16 +28,16 @@ const Transactions = () => {
     <div className='shadow bg-white rounded-[20px] p-[30px]'>
       <p className='text-[#333333] text-[20px] font-[700]'>Transactions</p>
       <div className='rounded-[8px] border border-[#DCDCDC] p-[30px] mt-3'>
-        <div className='flex items-center gap-[12px]'>
+        {/* <div className='flex items-center gap-[12px]'>
           {
             transactionsArray.map(transaction => (
               <p className='bg-[#EBEBEB] py-1 w-[100px] text-center cursor-pointer text-[#A1A1A1] text-[14px] rounded-full'>{transaction}</p>
             ))
           }
-        </div>
+        </div> */}
         <div className='mt-10'>
           <p className='text-[#101010]'>Search by name</p>
-          <input className='border outline-none border-[#C8C8C8] px-2 py-[6px] mt-[6px] rounded text-[14px]' type="text" placeholder='Customer Name' />
+          <input onChange={e => setSearchString(e.target.value)} className='border outline-none border-[#C8C8C8] px-2 py-[6px] mt-[6px] rounded text-[14px]' type="text" placeholder='Customer Name' />
         </div>
       </div>
       <div class="relative overflow-x-auto sm:rounded-lg mt-9">
@@ -47,20 +48,49 @@ const Transactions = () => {
                           Customers
                       </th>
                       <th scope="col" class="px-6 py-3">
-                          Status
-                      </th>
-                      <th scope="col" class="px-6 py-3">
                           Date
                       </th>
                       <th scope="col" class="px-6 py-3">
                           Amount
                       </th>
                       <th scope="col" class="px-6 py-3">
-                          MOP
+                          Transaction Type
+                      </th>
+                      <th scope="col" class="px-6 py-3">
+                          Transaction Description
+                      </th>
+                      <th scope="col" class="px-6 py-3">
+                          Balance
                       </th>
                   </tr>
               </thead>
               <tbody>
+                {
+                  allTransactions.filter(transaction => transaction?.wallet?.user?.first_name.toLowerCase().includes(searchString.toLowerCase()) || transaction?.wallet?.user?.last_name.toLowerCase().includes(searchString.toLowerCase()))
+                  .map(transaction => (
+
+                    <tr class="bg-white border-b cursor-pointer">
+                        <td class="pl-3 pr-6 py-4">
+                          {transaction?.wallet?.user?.first_name} {transaction?.wallet?.user?.last_name}
+                        </td>
+                        <td class="px-6 py-4">
+                          {new Date(transaction.created_at).toLocaleDateString()}
+                        </td>
+                        <td class='px-6 py-4 capitalize'>
+                          #{transaction.amount.toLocaleString()}
+                        </td>
+                        <td class='px-6 py-4 capitalize'>
+                          <span class={` py-1 px-6 rounded-full text-gray-100 ${transaction.transaction_type === 'debit' ? 'bg-red-300' : 'bg-green-300'}`}>{transaction.transaction_type}</span>
+                        </td>
+                        <td class="px-6 py-4 capitalize">
+                          {transaction.description}
+                        </td>
+                        <td class="px-6 py-4">
+                          #{transaction?.wallet?.balance?.toLocaleString()}
+                        </td>
+                    </tr>
+                  ))
+                }
                   {/* <tr class="bg-white border-b cursor-pointer">
                       <td class="pl-3 pr-6 py-4">
                         Tosin Olawale
